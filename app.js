062e72d4fc2e772f01bc5b699b0f08f08a8450be
shahzad <shazad.02@gmail.com>
2020-01-08 11:25:38 +0500

    set

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/chaapp');
 
 //access control for reactnative 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
